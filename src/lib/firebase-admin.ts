e9d28bcd4901d4aa8a3629c4016025a7d1ec7d5a
PRINCE JOHNSON <princeonejohnson@gmail.com>
2025-08-22 03:06:39 +0000

    Same error, this is my json file {   "type": "service_account",   "proje

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY;
     const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
     const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 
