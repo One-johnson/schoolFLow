b2a1d1a7bf0644e88e78a1794c532e03a113f05e
PRINCE JOHNSON <princeonejohnson@gmail.com>
2025-08-22 00:49:39 +0000

    is firebase installed,

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyDdEt90BYxkmlomk7AeOdRWfOXq7HY5O14",
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "schoolflow-731q1.firebaseapp.com",
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL: "https://schoolflow-731q1-default-rtdb.firebaseio.com",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "schoolflow-731q1",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "schoolflow-731q1.firebasestorage.app",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "349830314157",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:349830314157:web:e0bc3133825a7c34586e24",
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "",
+  }
 };
 
 export default nextConfig;
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,19 +4,19 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  projectId: "schoolflow-731q1",
-  appId: "1:349830314157:web:e0bc3133825a7c34586e24",
-  storageBucket: "schoolflow-731q1.firebasestorage.app",
-  apiKey: "AIzaSyDdEt90BYxkmlomk7AeOdRWfOXq7HY5O14",
-  authDomain: "schoolflow-731q1.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "349830314157",
-  databaseURL: "https://schoolflow-731q1-default-rtdb.firebaseio.com",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase for client-side
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
