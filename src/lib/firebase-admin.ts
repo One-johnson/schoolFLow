ea1533d1bedd74a98a7cf0d7dfa52f5f512140a0
PRINCE JOHNSON <princeonejohnson@gmail.com>
2025-08-29 16:12:18 +0000

    check the functions src index.ts file and tell me what it does

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,1 +1,9 @@
 // This file is no longer needed and will be removed.
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://schoolflow-731q1-default-rtdb.firebaseio.com"
+});
