2d31b9e8c36e8f0bbc58023ec77d430bea2cf102
PRINCE JOHNSON <princeonejohnson@gmail.com>
2025-09-10 22:44:06 +0000

    alright, but when the button is clicked, it does not toggle to icon only

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -170,7 +170,7 @@ const Sidebar = React.forwardRef<
     {
       side = "left",
       variant = "sidebar",
-      collapsible = "offcanvas",
+      collapsible = "icon",
       className,
       children,
       ...props
